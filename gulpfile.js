e3a01ade59b3a881838c226333243045ba7dc008
josecaos <jazzvoon@gmail.com>
2018-05-06 01:27:46 -0500

    Gulp watchers + browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 var gulp = require('gulp')
-
 var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
 
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
   // return gulp.src('scss/**/*.scss')//accept any file in scss folder
   .pipe(sass())
   .pipe(gulp.dest('css/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
+
+gulp.task('watch', ['browserSync','sass']/*order*/, function() {
+  gulp.watch('scss/**/*.scss',['sass'])
+  gulp.watch('sections/**/*.php', browserSync.reload); 
+  gulp.watch('js/**/*.js', browserSync.reload);
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      basedir: '/'
+    }
+  })
 })
