5c31958ac3e94b68f9a9288f9a94d8edf092c335
josecaos <jazzvoon@gmail.com>
2018-05-21 08:29:22 -0500

    Avances plantilla ICO

--- sections/home/ico.php
+++ sections/home/ico.php
@@ -16,7 +16,25 @@
       'data' => "$1 Eth = 3600 EBY Tokens",
     ),
     array(
-      'text' => "Total Supply 42,000,000 EBY Tokens",
+      'text' => "Total Supply ",
+      'data' => "42,000,000 EBY Tokens",
+    )
+  );
+  $ico_graph_title = "Points Scored";
+  $ico_graph_title_2 = "Bonus";
+  $ico_graph_text = "For certain amount of ether you will get a discount";
+  $bonus = array(
+    array(
+      'sent' => "If you send 5 ether ",
+      'discount' => "Your discount will be of 10%",
+    ),
+    array(
+      'sent' => "If you send 24 ether ",
+      'discount' => "Your discount will be of 20%",
+    ),
+    array(
+      'sent' => "If you send 50 ether ",
+      'discount' => "Your discount will be of 25%",
     )
   );
   ?>
@@ -47,12 +65,37 @@
 
     <?php endfor; ?>
 
-
+    <!--  -->
     <div class="ico-title col-12 text-center">
-    <h2 class="ico-text col-12 text-center">
-      <?php echo $ico_title; ?>
-    </h2>
-    <p class="col-12 text-center"><?php echo $ico_text_2; ?></p>
+      <h2 class="ico-text col-12 text-center">
+        <?php echo $ico_title; ?>
+      </h2>
+      <p class="col-12 text-center"><?php echo $ico_text_2; ?></p>
+    </div>
+
+    <!--  -->
+    <div class="ico-graph col-12">
+      <h2 class="ico-graph-title col-12 text-left">
+        <?php echo $ico_graph_title; ?>
+      </h2>
+      <div class="ico-graphic">
+
+      </div>
+      <h2 class="ico-graph-title col-12 text-center upper">
+        <?php echo $ico_graph_title_2; ?>
+      </h2>
+      <p class="ico-graph-text col-12 text-center"><?php echo $ico_graph_text; ?></p>
+      <div class="ico-graph-bonus">
+
+        <?php for ($i=0; $i < count($bonus) ; $i++):?>
+
+          <div class="bonus row col-12">
+            <p class="bonus-sent col-6 text-right"><?php echo $bonus[$i]['sent']; ?></p>
+            <p class="bonus-discount col-6 text-left"><?php echo $bonus[$i]['discount']; ?></p>
+          </div>
+
+        <?php endfor; ?>
+      </div>
     </div>
 
   </div>
